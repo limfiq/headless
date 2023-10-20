d1349ddcc6729d858e4ff4398e2dcbfd16a0ecd7
limfiq <mtaufiq39@gmail.com>
2023-10-20 13:33:03 +0700

    Upload Project
    Upload Project Backend dan Frontend awal

---
+++ frontend/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Auth() {
+  return (
+    <div>
+      <div>
+      <form action="#">
+        <input type="text" placeholder='username' />
+        <input type="password" placeholder='password' />
+        <button type="submit">Login</button>
+        </form>        
+        </div>
+     </div>
+  )
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <header>Limfiq | Blog </header>
+        <main>
+          
+        </main>
+        <footer>
+          <div>
+            <p>Copyright 2023</p>
+          </div>
+        </footer>
+        
+      </body>
+      </html>
+    </div>
+  )
+}
