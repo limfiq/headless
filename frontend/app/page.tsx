5b24e07862c22436790cac7176aed9f3c87331de
limfiq <mtaufiq39@gmail.com>
2023-10-23 13:32:19 +0700

    Update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,24 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>CRUD Next - Strapi</title>
-      </head>
-      <body>
-        <header>Pilih Metode </header>
-        <main>
-            <button type='button' title='Fetching'/>
-        </main>
-        <footer>
-          <div>
-            <p>Copyright 2023</p>
-          </div>
-        </footer>
-        
-      </body>
-      </html>
+      
     </div>
   )
 }
