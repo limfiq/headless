17fab160ae2281c783b9512e7774528fa798f4c6
limfiq <mtaufiq39@gmail.com>
2023-10-27 22:06:19 +0700

    Update routing
    Routing Antar Halaman

--- frontend/app/axios/page.tsx
+++ frontend/app/axios/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
 
+import React from 'react'
+import Link from 'next/link'
 export default function AxiosPage() {
   return (
-    <h1>Ini Halaman Axios</h1>
+    <div>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/fetch">Fetch</Link></li>
+        <li><Link href="/axios">Axios</Link></li>
+      </ul>
+     
+      <h1>Ini Halaman Axios</h1>
+    </div>
   )
 }
--- frontend/app/fetch/page.tsx
+++ frontend/app/fetch/page.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+import { Imprima } from 'next/font/google'
 import React from 'react'
 
 export default function Fetchpage() {
   return (
-    <h1>Ini Halaman Fetch</h1>
+    <div>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/fetch">Fetch</Link></li>
+        <li><Link href="/axios">Axios</Link></li>
+      </ul>
+      <h1>Ini Halaman Fetch</h1>
+      
+    </div>
   )
 }
